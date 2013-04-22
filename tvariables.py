c9f9456b214952eab9e5d6038d4795f5e43c967a
gchapa22 <gerardo.chapa.q@gmail.com>
2013-04-21 21:09:24 -0500

    Update tvariables.py

--- tvariables.py
+++ tvariables.py
@@ -9,6 +9,7 @@
 #----------------------------------------------
 
 import sys
+from cuadruplos import * 
 
 class TablaVariableNodo:
 	def __init__(self, nombre, tipo, dire):
@@ -22,23 +23,48 @@ class TablaProcedimientoNodo:
 		self.tipo_retorno = tipo
 		self.dir_base = dirb
 		self.var = []
+		self.param = []
 		self.se_uso = False
 
-tabla_pro = [ ]	
+tabla_pro = [ ]
 
-def insert_procedimiento(nombre, tipo, dirb):
+def def_proc_1(nombre, tipo, dirb):
 	global tabla_pro
 	existe_pro (nombre)
 	pro = TablaProcedimientoNodo(nombre, tipo, dirb)
 	tabla_pro.append(pro)
+
+def def_proc_2(nombre, tipo, dire, proc):
+	global tabla_pro
+	existe_param(nombre,proc)
+	param = TablaVariableNodo(nombre, tipo, dire)
+	for n,pro in enumerate(tabla_pro):
+		if pro.nombre_funcion == proc:
+			tabla_pro[n].param.append(param)
+
+def def_proc_3(n, cont_saltos):
+	global tabla_pro
+	tabla_pro[n].dir_base = cont_saltos
+	pass
+
+def def_proc_4():
+	global tabla_pro
+	cuadruplo = Cuadruplo("ENDPROC", "", "", "")
+	insert_cuadruplo(cuadruplo)
+	pass
+
 
 def print_tables(currentProList):
 	print "Tabla de procedimientos y variables"
 	for currentPro in currentProList:
 		if currentPro:
 			print currentPro.nombre_funcion, " - ", currentPro.tipo_retorno, " - ", currentPro.dir_base
+			print "Vars"
 			for variable in currentPro.var:
 				print variable.nombre_variable, " - ", variable.tipo_dato, " - ", variable.direccion
+			print "Params"
+			for param in currentPro.param:
+				print param.nombre_variable, " - ", param.tipo_dato, " - ", param.direccion
 			print "\n"
 		else:
 			print "List is empty"
@@ -46,7 +72,7 @@ def print_tables(currentProList):
 
 def insert_variable(nombre, tipo, dire, proc):
 	global tabla_pro
-	existe_var (nombre,proc)
+	existe_var(nombre,proc)
 	variable = TablaVariableNodo(nombre, tipo, dire)
 	for n,pro in enumerate(tabla_pro):
 		if pro.nombre_funcion == proc:
@@ -70,6 +96,16 @@ def existe_var(nombre,proc):
 					sys.exit()
 	pass
 
+def existe_param(nombre,proc):
+	global tabla_pro
+	for n,pro in enumerate(tabla_pro):
+		if pro.nombre_funcion == proc:
+			for param in pro.param:
+				if param.nombre_variable == nombre:
+					print "Sorry - the parameter %s already exist"%nombre
+					sys.exit()
+	pass
+
 def busca_tipo(nombre,proc):
 	global tabla_pro
 	esta = False
@@ -80,9 +116,16 @@ def busca_tipo(nombre,proc):
 					tipo_var  = variable.tipo_dato
 					esta = True
 					return tipo_var
-			if not esta:
-				print "Sorry - the variable  %s was not declared"%nombre
- 				sys.exit()
+ 	for n,pro in enumerate(tabla_pro):
+		if pro.nombre_funcion == "Global":
+			for variable in pro.var:
+				if variable.nombre_variable == nombre:
+					tipo_var  = variable.tipo_dato
+					esta = True
+					return tipo_var
+	if not esta:
+		print "Sorry - the variable  %s was not declared"%nombre
+ 		sys.exit()
 
 	pass
 
@@ -96,9 +139,16 @@ def get_address(nombre,proc):
 					address_var  = variable.direccion
 					esta = True
 					return address_var
-			if not esta:
-				print "Sorry - the variable  %s was not declared"%nombre
- 				sys.exit()
+	for n,pro in enumerate(tabla_pro):
+		if pro.nombre_funcion == "Global":
+			for variable in pro.var:
+				if variable.nombre_variable == nombre:
+					address_var  = variable.direccion
+					esta = True
+					return address_var
+	if not esta:
+		print "Sorry - the variable  %s was not declared"%nombre
+ 		sys.exit()
 
 	pass
 
